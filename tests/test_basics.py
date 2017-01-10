763afbce1802a45e4c4d6eec2fccf6012e9e9c16
Robert Hardy <robbert@gmail.com>
2017-01-10 20:14:44 +0000

    Update test to use a Pandas data frame.
    Fails, wip.

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,16 @@
 import unittest
 
 
+import numpy as np
+import pandas as pd
+
+
 class TestSomething(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.DataFrame.from_records(
+            np.rec.array([(0, 1, 'a'), (1, 2, 'b'), (2, 3, 'c'), (3, 4, 'd')],
+            dtype=[('index', '<i8'), ('one', '<i8'), ('two', 'O')])
+        )
+
     def test_one(self):
-        self.assertEqual(1, 2)
+        self.assertEqual(len(self.df), 3)
